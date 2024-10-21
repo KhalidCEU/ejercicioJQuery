8a9b6b540ed237011a25380652ce4f848e3e008a
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-10-21 14:18:29 +0200

    refactor: fix typos

--- validate.js
+++ validate.js
@@ -13,13 +13,13 @@ function validateForm() {
 
     const phoneNumber = $("#phoneNumber").val().trim();
     if (!/^\d+$/.test(phoneNumber)) {
-        alert("Please enter a valid phone number (only numbers,no spaces).");
+        alert("Please enter a valid phone number (only numbers, no spaces).");
         return false;
     }
 
     const password = $("#password").val();
     if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/.test(password)) {
-        alert("The password must have at least 8 characters, including at least 1 uppercase letter, 1 lowerrcase letter and 1 number.");
+        alert("The password must have at least 8 characters, including at least 1 uppercase letter, 1 lowercase letter and 1 number.");
         return false;
     }
 
